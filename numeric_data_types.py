11b13cba6a0fa4dd1c98ebda27e140965f926a2e
gbantar <78503597+gbantar@users.noreply.github.com>
2021-03-17 23:14:53 -0400

    Add files via upload

---
+++ numeric_data_types.py
@@ -0,0 +1,17 @@
+print("Sarah Mary Beth Gwei")
+# Numeric Data Types
+myValue=96
+print(str(myValue) + " is of data type " + str(type(myValue)))
+myValue=True
+print(str(myValue) + " is of data type " + str(type(myValue)))
+myValue=5j
+print(str(myValue) + " is of data type " + str(type(myValue)))
+myValue=3.4
+myValue=False
+print(str(myValue) + " is of data type " + str(type(myValue)))
+x=MyValue1 + Myvalue2
+print (X)
+
+
+
+
