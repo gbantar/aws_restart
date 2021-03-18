eb4de2d831c5f72649b92b15bd7342f72e47156e
gbantar <78503597+gbantar@users.noreply.github.com>
2021-03-17 23:19:07 -0400

    Update and rename numeric_data_types.py to Writing Python code using the string data type.py

---
+++ Writing Python code using the string data type.py
@@ -0,0 +1,15 @@
+myString="Welcome to my beginner coding tutorials."
+print(myString)
+print(type(myString))
+print(myString + " sit back and relax as we are about to begin " + str(type(myString)))
+lesson1= "we will begin with intro to python" 
+lesson2=" and then write our first code"
+lesson3=lesson1+lesson2
+print(lesson3)
+section2= ("I am tired for today please don't leave as we will continue tomorrow")
+print (section2)
+section3= ("see you tommorrow, ciao ciao")
+print(section3)
+
+
+
